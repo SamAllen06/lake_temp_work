6726a5e6f5ffb7470b7896f03207de3cba06a4ed
Aidan Bensch <57157106+GamemakerPixel@users.noreply.github.com>
2024-05-06 13:30:35 -0400

    Allow cli to read FUT_lake_range.txt with range_reader.py

--- lake_temperature_mapper/scripts/cli.py
+++ lake_temperature_mapper/scripts/cli.py
@@ -1,18 +1,26 @@
 import argparse
 
 from binary_runner import BinaryRunner
+from range_reader import RangeReader
 
 
 def initialize_cli_arguments():
     parser = argparse.ArgumentParser()
+
     parser.add_argument("elmtest_path", help="The path to elmtest.exe.", type=str)
+    parser.add_argument("range_path", help="The path to FUT_lake_range.txt.", type=str)
+
     return parser.parse_args()
 
 
 def main():
     args = initialize_cli_arguments()
+
     runner = BinaryRunner(args.elmtest_path)
     runner.run()
+
+    range_reader = RangeReader(args.range_path)
+    print(range_reader.read_ranges())
 
 
 if __name__ == "__main__":
---
+++ lake_temperature_mapper/scripts/range_reader.py
@@ -0,0 +1,28 @@
+import os
+from typing import TextIO, Mapping
+
+
+class RangeReader:
+    RANGE_DELIMITER = " - "
+
+    def __init__(self, file_path: os.PathLike):
+        self.file_path = file_path
+
+    def read_ranges(self) -> Mapping[str, tuple[float, float]]:
+        ranges = {}
+
+        with open(self.file_path, "r") as file:
+            lines = file.readlines()
+
+        for parameter_index in range(len(lines) // 2):
+            line_index = parameter_index * 2
+
+            parameter_name = lines[line_index].strip()
+            range_strings = lines[line_index + 1].split(self.RANGE_DELIMITER)
+
+            min = float(range_strings[0])
+            max = float(range_strings[1])
+
+            ranges[parameter_name] = (min, max)
+
+        return ranges
