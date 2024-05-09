89d9fd646089d41977de6e051548b247fe16d52f
Aidan Bensch <57157106+GamemakerPixel@users.noreply.github.com>
2024-05-09 12:25:13 -0400

    Move scripts not meant to be run by a user into mapping directory

--- lake_temperature_mapper/scripts/cli.py
+++ lake_temperature_mapper/scripts/cli.py
@@ -1,25 +1,21 @@
 import argparse
-import os
+from pathlib import Path
 
-from binary_runner import BinaryRunner
-from difference_analyzer import DifferenceAnalyzer
-from param_editor import ParamEditor
-from range_reader import RangeReader
+from mapping.binary_runner import BinaryRunner
+from mapping.config_reader import ConfigReader
+from mapping.difference_analyzer import DifferenceAnalyzer
+from mapping.param_editor import ParamEditor
+from mapping.range_reader import RangeReader
+
+PARENT_DIRECTORY = Path(__file__).resolve().parent
+DEFAULT_CONFIG_PATH = PARENT_DIRECTORY / "config" / "mapper.conf"
 
 
 def initialize_cli_arguments():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("elmtest_path", help="The path to elmtest.exe.",
-                        type=str)
-    parser.add_argument("range_path", help="The path to FUT_lake_range.txt.",
-                        type=str)
-    parser.add_argument("params_path", help="The path to lakeparams.txt.",
-                        type=str)
-    parser.add_argument("-r", "--reference_path", help="The path to the reference output file.",
-                        type=str, default="cpu_LakeTemperatureRef.txt")
-    parser.add_argument("-t", "--test_path", help="The path to the test output file.",
-                        type=str, default="cpu_LakeTemperatureTest.txt")
+    parser.add_argument("-c", "--config_path", help="The path to the config file. Default: scripts/config/mapper.conf",
+                        type=str, default="")
 
     return parser.parse_args()
 
@@ -27,20 +23,28 @@ def initialize_cli_arguments():
 def main():
     args = initialize_cli_arguments()
 
-    script_directory = os.path.dirname(__file__)
+    if not args.config_path:
+        absolute_config_path = DEFAULT_CONFIG_PATH
+    else:
+        absolute_config_path = Path.cwd() / args.config_path
 
-    runner = BinaryRunner(os.path.join(script_directory, args.elmtest_path))
+    config_reader = ConfigReader(absolute_config_path)
+
+    if not config_reader.config_exists():
+        raise FileNotFoundError(f"Could not find config file {absolute_config_path}.")
+
+    runner = BinaryRunner(PARENT_DIRECTORY / config_reader.get_path_of("binary_path"))
     runner.run()
 
-    range_reader = RangeReader(os.path.join(script_directory, args.range_path))
+    range_reader = RangeReader(PARENT_DIRECTORY / config_reader.get_path_of("range_path"))
     print(range_reader.read_ranges())
 
-    param_editor = ParamEditor(os.path.join(script_directory, args.params_path))
+    param_editor = ParamEditor(PARENT_DIRECTORY / config_reader.get_path_of("params_path"))
     param_editor.modify_parameter("betavis", 0.5)
 
     difference_analyzer = DifferenceAnalyzer(
-        os.path.join(script_directory, args.reference_path),
-        os.path.join(script_directory, args.test_path)
+        PARENT_DIRECTORY / config_reader.get_path_of("ref_output"),
+        PARENT_DIRECTORY / config_reader.get_path_of("test_output")
     )
 
     print(difference_analyzer.compare_outputs())
---
+++ lake_temperature_mapper/scripts/mapping/config_reader.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+
+
+class ConfigReader:
+    def __init__(self, config_path: Path):
+        self._config_path = config_path
+        self._config_map = self._read_config()
+    
+    def config_exists(self):
+        return self._config_path.exists() and self._config_path.is_file()
+
+    def get_path_of(self, key: str):
+        return self._config_map[key]
+
+    def _read_config(self):
+        if not self.config_exists():
+            return {}
+        
+        with open(self._config_path, "r") as config_file:
+            lines = config_file.readlines()
+
+        selections = {}
+
+        for line in lines:
+            split_line = line.strip().split(": ")
+            selections[split_line[0]] = Path(split_line[1])
+
+        return selections
+
+
+
