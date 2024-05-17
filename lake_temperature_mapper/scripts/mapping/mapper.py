db180ac7dcb267e91e560c97fab789bed8c9b12a
Aidan Bensch <57157106+GamemakerPixel@users.noreply.github.com>
2024-05-17 14:16:07 -0400

    Correct instance variable naming in Mapper

--- lake_temperature_mapper/scripts/mapping/mapper.py
+++ lake_temperature_mapper/scripts/mapping/mapper.py
@@ -17,22 +17,22 @@ class Mapper:
             config_reader: ConfigReader,
             output_writer: OutputWriter
     ):
-        self.config_reader = config_reader
-        self.output_writer = output_writer
-        self.defaults_writer = DefaultsWriter(
-            self.config_reader.get_path("defaults_path"),
-            self.config_reader.get_path("params_path")
+        self._config_reader = config_reader
+        self._output_writer = output_writer
+        self._defaults_writer = DefaultsWriter(
+            self._config_reader.get_path("defaults_path"),
+            self._config_reader.get_path("params_path")
         )
-        self.param_editor = ParamEditor(
-            self.config_reader.get_path("params_path")
+        self._param_editor = ParamEditor(
+            self._config_reader.get_path("params_path")
         )
-        self.binary_runner = BinaryRunner(
-            self.config_reader.get_path("binary_path"),
-            self.config_reader.get("binary_args")
+        self._binary_runner = BinaryRunner(
+            self._config_reader.get_path("binary_path"),
+            self._config_reader.get("binary_args")
         )
-        self.difference_analyzer = DifferenceAnalyzer(
-            self.config_reader.get_path("ref_output"),
-            self.config_reader.get_path("test_output")
+        self._difference_analyzer = DifferenceAnalyzer(
+            self._config_reader.get_path("ref_output"),
+            self._config_reader.get_path("test_output")
         )
 
     def map(self):
@@ -44,8 +44,8 @@ class Mapper:
 
     def _read_orders(self) -> list[Order]:
         order_reader = OrderReader(
-            self.config_reader.get_path("order_directory"),
-            self.config_reader.get_path("range_path")
+            self._config_reader.get_path("order_directory"),
+            self._config_reader.get_path("range_path")
         )
 
         return order_reader.read_orders()
@@ -54,19 +54,19 @@ class Mapper:
         for order in orders:
             self._execute_order(order)
 
-        self.defaults_writer.write_defaults()
-        self.output_writer.finish()
+        self._defaults_writer.write_defaults()
+        self._output_writer.finish()
 
     def _execute_order(self, order: Order) -> None:
-        self.defaults_writer.write_defaults()
+        self._defaults_writer.write_defaults()
         sampler = Sampler(order.start, order.end, order.sample_count)
-        self.output_writer.write_order_header(order)
+        self._output_writer.write_order_header(order)
         for sample_value in sampler.get_samples():
-            self.output_writer.write_parameter_sample(
+            self._output_writer.write_parameter_sample(
                 order.parameter,
                 sample_value
             )
-            self.param_editor.modify_parameter(order.parameter, sample_value)
-            self.binary_runner.run_binary()
-            difference_map = self.difference_analyzer.compare_outputs()
-            self.output_writer.write_difference_map(difference_map)
+            self._param_editor.modify_parameter(order.parameter, sample_value)
+            self._binary_runner.run_binary()
+            difference_map = self._difference_analyzer.compare_outputs()
+            self._output_writer.write_difference_map(difference_map)
