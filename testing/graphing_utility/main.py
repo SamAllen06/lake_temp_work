b47f5a8e0e2c2a3aafc009b1a48c4b46081faa67
Aidan Bensch <57157106+GamemakerPixel@users.noreply.github.com>
2025-10-14 00:48:08 -0400

    Complete 2d simple plotting in graphing utility

--- testing/graphing_utility/main.py
+++ testing/graphing_utility/main.py
@@ -4,6 +4,7 @@ import sys
 
 from netCDF4 import Dataset
 import numpy as np
+from matplotlib import pyplot
 
 
 def main() -> None:
@@ -16,10 +17,9 @@ def main() -> None:
 
     with Dataset(dataset_path, "r", format="NETCDF4") as dataset:
         input_data = _get_input_data(dataset, args.input)
-        output_data = _get_output_data(dataset, args.output, args.output_index)
+        output_data = _get_output_data(dataset, args.input, args.output, args.output_index)
 
-        print("In: " + str(input_data))
-        print("Out: " + str(output_data))
+    _plot_data(input_data, output_data)
 
 
 def _parse_args() -> Namespace:
@@ -37,15 +37,26 @@ def _get_input_data(dataset: Dataset, input: str) -> np.array:
         print(f"Unable to find input {input} in dataset", file=sys.stderr)
         sys.exit(1)
 
-    return dataset.variables[input]
+    return dataset.variables[input][:]
 
-def _get_output_data(dataset: Dataset, output: str, index: int) -> np.array:
+def _get_output_data(dataset: Dataset, input:str, output: str, index: int) -> np.array:
     if output not in dataset.variables:
         print(f"Unable to find output {output} in dataset", file=sys.stderr)
         sys.exit(1)
 
-    return dataset.variables[output][:, :, index]
+    input_index = dataset.variables[output].dimensions.index(input)
+    output_slice_list = [0] * len(dataset.variables[output].dimensions)
+    output_slice_list[input_index] = slice(None)
+    output_slice_list[-1] = index
+    output_slice = tuple(output_slice_list)
 
+    return dataset.variables[output][output_slice]
+
+def _plot_data(input_data: np.array, output_data: np.array) -> None:
+    figure, axes = pyplot.subplots()
+    axes.plot(input_data, output_data)
+    axes.scatter(input_data, output_data)
+    pyplot.show()
 
 if __name__ == "__main__":
     main()
