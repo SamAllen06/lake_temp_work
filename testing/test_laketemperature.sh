10c2f78e8b2b7d28a121e9bc8ce252cbba8541c6
Aidan Bensch <57157106+GamemakerPixel@users.noreply.github.com>
2025-11-11 09:50:23 -0500

    Create docker image for running elmtest

--- testing/test_laketemperature.sh
+++ testing/test_laketemperature.sh
@@ -1,7 +1,12 @@
 #! /usr/bin/env bash
 
-docker buildx build . -t lake_temp \
-	&& docker run --name lake_temp -it lake_temp \
-	&& docker rm lake_temp \
-	&& docker rmi lake_temp
+docker buildx build . -t lake_temp
+
+if [ $? -eq 0 ]; then
+	docker run --name lake_temp -it lake_temp 
+	if [ $? -eq 0 ]; then
+		docker rm lake_temp
+	fi
+	docker rmi lake_temp
+fi
 
