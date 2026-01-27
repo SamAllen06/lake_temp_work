c12a314c0d6ed04100ba1a386566556af3b08099
Aidan Bensch <57157106+GamemakerPixel@users.noreply.github.com>
2026-01-26 22:05:58 -0500

    Make lake_temp Dockerfile compile elmtest and clean up testing directory

--- testing/test_compile/compile_lake_temp.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-docker buildx build -t lake_temp_compile ./
-docker run --name lake_temp_compile --volume ./result:/mnt/result lake_temp_compile
-docker rm lake_temp_compile
-docker rmi lake_temp_compile
