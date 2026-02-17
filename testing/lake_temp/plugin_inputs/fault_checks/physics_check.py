a0c60806620948670575884521053c3685bd3115
Aidan Bensch <aidanbensch@gmail.com>
2026-02-17 11:57:15 -0500

    Add physics checks using a constant, check-defined tfrz

--- testing/lake_temp/plugin_inputs/fault_checks/physics_check.py
+++ testing/lake_temp/plugin_inputs/fault_checks/physics_check.py
@@ -1,27 +1,35 @@
 import numpy as np
 import numpy.typing as npt
 
-
 # tfrz not in constants.
-#def check_temp_at_freezing_where_lake_is_frozen(
+TFRZ = 273.15
+
+
+def check_temp_at_freezing_where_lake_is_frozen(
 #    tfrz: float,
-#    test_col_es_t_lake: npt.NDArray,
-#    test_lakestate_vars_lake_icefrac_col: npt.NDArray,
-#) -> None:
-#    diff_to_frozen = np.abs(test_lakestate_vars_lake_icefrac_col - 1.0)
-#    frozen = diff_to_frozen <= 1E-6
-#
-#    diff_to_freezing_temp = np.abs(tfrz - test_col_es_t_lake)
-#    close_to_freezing = diff_to_freezing_temp <= 1E-3
-#
-#    # frozen -> freezing
-#    # not freezing -> not frozen
-#    # 0 0 | 1
-#    # 0 1 | 1
-#    # 1 0 | 0
-#    # 1 1 | 1
-#
-#    frozen_and_freezing = frozen & close_to_freezing
-#    not_frozen_or_frozen_and_freezing = frozen_and_freezing | ~frozen
-#
-#    assert np.all(not_frozen_or_frozen_and_freezing), "Columns are frozen but not at freezing point"
+    test_col_es_t_lake: npt.NDArray,
+    test_lakestate_vars_lake_icefrac_col: npt.NDArray,
+) -> None:
+    diff_to_frozen = np.abs(test_lakestate_vars_lake_icefrac_col - 1.0)
+    frozen = diff_to_frozen <= 1E-6
+
+    diff_to_freezing_temp = np.abs(TFRZ - test_col_es_t_lake)
+    close_to_freezing = diff_to_freezing_temp <= 1E-3
+
+    frozen_and_freezing = frozen & close_to_freezing
+    not_frozen_or_frozen_and_freezing = frozen_and_freezing | ~frozen
+
+    assert np.all(not_frozen_or_frozen_and_freezing), "Columns are frozen but not at freezing point"
+
+
+def no_eddies_when_surface_frozen(
+    test_col_es_t_lake: npt.NDArray,
+    test_lakestate_vars_savedtke1_col: npt.NDArray,
+) -> None:
+    surface_frozen = test_col_es_t_lake[:, 1, :] <= TFRZ
+
+    eddies_present = test_lakestate_vars_savedtke1_col > 0.0
+
+    frozen_and_eddies = surface_frozen & eddies_present
+
+    assert not np.any(frozen_and_eddies), "Eddies are present on a frozen surface"
