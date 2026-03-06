aa8579fbe8fe41b77d09d1bc59fc95e3104abc76
Aidan Bensch <aidanbensch@gmail.com>
2026-03-06 18:19:09 -0500

    Add new MockModel check for testing SKIPPED status

--- testing/mock_model/plugin_inputs/fault_checks/sum_check.py
+++ testing/mock_model/plugin_inputs/fault_checks/sum_check.py
@@ -1,11 +1,17 @@
 import numpy as np
+import numpy.typing as npt
+
+from mtf_fault_finding import CheckStatus
 
 
-def check_passes(test_fakevar_sum: list[float]):
+def check_passes(test_fakevar_sum: npt.NDArray):
     assert np.all(test_fakevar_sum > 0), f"fakevar_sum expected to be positive, was {test_fakevar_sum}"
 
-def check_fails(test_fakevar_product: list[float]):
+def check_skips(test_fakevar_product: npt.NDArray):
+    return CheckStatus.SKIPPED
+
+def check_fails(test_fakevar_product: npt.NDArray):
     assert np.all(test_fakevar_product < 0), f"fakevar_product expected to be negative, was {test_fakevar_product}"
 
-def check_errors(test_fakevar_sum: list[float]):
+def check_errors(test_fakevar_sum: npt.NDArray):
     raise ValueError("boof")
