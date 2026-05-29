9a7021491744db4e450f65383be625e700d01dbd
SamAllen06 <samgallen06@gmail.com>
2026-05-29 14:05:10 -0400

    Update check_errsoi_almost_zero to use absolute value
    The check document just said it should be close to zero but the output document specified the absolute value should be close to zero

--- testing/lake_temp/plugin_inputs/fault_checks/energy_residual_handling_check.py
+++ testing/lake_temp/plugin_inputs/fault_checks/energy_residual_handling_check.py
@@ -13,5 +13,4 @@ def check_errsoi_almost_zero(
     test_col_ef_errsoi = NonFiniteValuesHandler.mask_non_finite_values(
         test_col_ef_errsoi)
 
-    assert np.all(test_col_ef_errsoi <= 1E-6), "Error is above tolerance"
-
+    assert np.all(np.abs(test_col_ef_errsoi) <= 1E-6), "Error is above tolerance"
